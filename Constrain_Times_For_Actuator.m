6a6c7f139b1982d4102dbdad6f94cdcff64f72ed
wtharvey2 <55966891+wtharvey2@users.noreply.github.com>
2020-06-06 15:18:20 -0700

    Removed Constrain_Times
    Since we're not worrying about the actuator constraint in the project.

--- Constrain_Times_For_Actuator.m
+++
@@ -1,7 +0,0 @@
-function [final_times] = Constrain_Times_For_Actuator(new_times)
-%%%%%%%%%%%% TO DO %%%%%%%%%%%%%%%%%%%%%%
-%% TO DO: ADD IN CONSTRAINT CHECKS FOR MAX DYNAMIC VALUES
-
-final_times = new_times;
-end
-
